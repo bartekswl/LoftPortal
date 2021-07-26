f7670dfd7f046a01985618c593fe07448c1d4967
Bart <bartekswl@gmail.com>
2021-07-26 04:37:32 +0100

    propert models added, admin configured

--- property/admin.py
+++ property/admin.py
@@ -1,7 +1,72 @@
 from django.contrib import admin
-from .models import Flat, Tenant
+from .models import Flat, Tenant, CommercialUnit, Concierge
 
 
 
-admin.site.register(Flat)
-admin.site.register(Tenant)
+class FlatAdmin(admin.ModelAdmin):
+    def has_delete_permission(self, request, obj=None):
+        if obj is not None:
+            if request.user.is_superuser:
+                return True
+            else:
+                return False
+
+    def has_change_permission(self, request, obj=None):
+        if obj is not None:
+            if request.user.is_superuser:
+                return True
+            else:
+                return False
+
+    def has_add_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+        else:
+            return False
+
+    list_display = ('flat_number', 'building', 'core', 'floor', 'flat_type', 'agency')
+    list_display_links = ('flat_number', 'building', 'core')
+    
+    ordering = ('flat_number',)
+
+    filter_horizontal = ()
+    list_filter = ('building','core', 'floor', 'flat_type')
+    fieldsets = ()
+
+
+class TenantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname', 'flat', 'status', 'date_added')
+
+
+
+class ConciergeAdmin(admin.ModelAdmin):
+    def has_delete_permission(self, request, obj=None):
+        if obj is not None:
+            if request.user.is_superuser:
+                return True
+            else:
+                return False
+
+    def has_add_permission(self, request, obj=None):
+        if obj is not None:
+            if request.user.is_superuser:
+                return True
+            else:
+                return False
+
+    def has_change_permission(self, request, obj=None):
+        if obj is not None:
+            if request.user.is_superuser:
+                return True
+            else:
+                return False
+
+    list_display = ('name', 'surname', 'work_pattern', 'phone_number')
+
+
+
+
+admin.site.register(Flat, FlatAdmin)
+admin.site.register(Tenant, TenantAdmin)
+admin.site.register(CommercialUnit)
+admin.site.register(Concierge, ConciergeAdmin)
--- property/models.py
+++ property/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-# from django.core.exceptions import ValidationError
-# from django.utils.translation import gettext_lazy as _
 
+from property.flat_codes import flat_codes
 
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+#validators=[validate_even]
 # def validate_even(value):
 #     if value != 'Cassia':
 #         raise ValidationError(
@@ -11,8 +14,9 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 #             params={'value': value},
 #         )
 
-flat_codes = (('C1', 'C1'), ('C2','C2'), ('C3', 'C3'),('R1', 'R1'), ('R2','R2'), ('R3', 'R3'))
+
 building_codes = (('Cassia', 'Cassia'),('Rosewood', 'Rosewood'))
+own_flat_type = (('Vonder', 'Vonder'),('Anglo', 'Anglo'), ('Private', 'Private'), ('Other', 'Other'))
 
 class Flat(models.Model):
     
@@ -21,9 +25,13 @@ class Flat(models.Model):
     core           = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], null=True, blank=False)
     floor          = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(8)], null=True, blank=False)
     flat_size      = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(3)], null=True, blank=False)
-    flat_type      = models.CharField(max_length=10, null=True, blank=False)
+    full_address   = models.CharField(max_length=40, null=True, blank=True)
+    flat_type      = models.CharField(max_length=10, choices=own_flat_type, null=True, blank=False)
+    agency         = models.CharField(max_length=20, null=True, blank=True)
+    agency_contact = models.CharField(max_length=50, null=True, blank=True)
     auth_access    = models.CharField(max_length=100, blank=True)
     additional_notes= models.TextField(max_length=300, blank=True)
+    is_empty       = models.BooleanField(default=False)
     
     
     
@@ -33,18 +41,22 @@ class Flat(models.Model):
 
 
 
+tenancy = (('Tenant', 'Tenant'),('Owner', 'Owner'), ('Other', 'Other'))
 
 class Tenant(models.Model):
 
     name          = models.CharField(max_length=15, null=True, blank=False)
     surname       = models.CharField(max_length=30, null=True, blank=False)
-    email         = models.EmailField(max_length=100, unique=True)
+    email         = models.EmailField(max_length=50, unique=True)
     phone_number  = models.CharField(max_length=20, null=True, blank=True, unique=True)
     flat          = models.ForeignKey(Flat, on_delete=models.PROTECT)
-    status        = models.CharField(max_length=10, null=True, blank=False)
+    status        = models.CharField(max_length=10, choices=tenancy, null=True, blank=False)
     stay_length   = models.CharField(max_length=20, null=True, blank=False)
     pet_licence   = models.CharField(max_length=20, null=True, blank=False)
     additional_notes= models.TextField(max_length=300, blank=True)
+    date_added    = models.DateField(auto_now_add=True, null=True)
+    moved_out     = models.BooleanField(default=False)
+    date_moved_out= models.DateField(auto_now_add=False, null=True)
 
     class Meta:
         unique_together = ('name', 'surname',)
@@ -52,3 +64,41 @@ class Tenant(models.Model):
 
     def __str__(self):
         return f'{self.name} {self.surname}' 
+
+
+
+
+
+class CommercialUnit(models.Model):
+
+    unit_number        = models.CharField(max_length=5, null=True, blank=False, unique=True)
+    business_name      = models.CharField(max_length=20, null=True, blank=False)
+    street_location    = models.CharField(max_length=20, null=True, blank=False)
+    full_address       = models.CharField(max_length=40, null=True, blank=True)
+    contact_name       = models.CharField(max_length=30, null=True, blank=True)
+    contact_email      = models.EmailField(max_length=50, null=True, blank=True, unique=True)
+    contact_number     = models.CharField(max_length=20, null=True, blank=True, unique=True)
+    is_running         = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.unit_number
+
+
+
+
+
+
+work_choices = (('Day', 'Day'),('Night', 'Night'), ('Other', 'Other'))
+
+class Concierge(models.Model):
+
+    name            = models.CharField(max_length=15, null=True, blank=False, unique=True)
+    surname         = models.CharField(max_length=15, null=True, blank=True)
+    work_pattern    = models.CharField(max_length=6, choices=work_choices, null=True, blank=False)
+    phone_number    = models.CharField(max_length=20, null=True, blank=True, unique=True)
+
+    def __str__(self):
+        return self.name
+
+   
